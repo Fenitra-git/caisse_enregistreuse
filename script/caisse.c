8c3249050a858b6c69a886dfde67a5c1e1d00207
fenitra <refenitra@student.udm.mu.ac>
2025-04-16 20:54:40 +0400

    version final 0.2

--- script/caisse.c
+++ script/caisse.c
@@ -4,7 +4,10 @@
 
 void monnaie(double r) {
     double b[] = {2000,1000,500,200,100,50,25,20,10,5,1,0.50,0.20,0.05};
-    char *n[] = {"Rs2000","Rs1000","Rs500","Rs200","Rs100","Rs50","Rs25","Rs20","Rs10","Rs5","Rs1","50 sous","20 sous","5 sous"};
+    char *n[] = {"Rs2000","Rs1000","Rs500","Rs200","Rs100","Rs50","Rs25",
+                    "Rs20","Rs10","Rs5","Rs1","50 sous","20 sous","5 sous"};
+    printf("Monnaie à rendre: %.2f Rs\n", r);
+    printf("Détails de votre monnaie:\n");
     for(int i = 0; i < 14; i++) {
         if(r <= 0)
             break;
@@ -22,15 +25,17 @@ int main() {
     double p;
     printf("Montant à payer: %.2f Rs\n", a);
     do {
-        printf("Argent donné: RS");
+        printf("Le montant que vous allez donner: ");
         scanf("%lf", &p);
         if(p < a) {
             printf("Montant insuffisant!\n");
         }
     } while(p < a);
-    
+
     if(p > a) {
         monnaie(p - a);
+    } else {
+        printf("Pas de monnaie à rendre.\n");
     }
     return 0;
 }
