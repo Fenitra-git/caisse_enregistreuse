54d9c4bea3ad5bf80c7d135b5f1ba745b52f4f4b
fenitra <refenitra@student.udm.mu.ac>
2025-04-16 21:15:53 +0400

    version 0.3

--- script/caisse.c
+++ script/caisse.c
@@ -2,40 +2,88 @@
 #include <stdlib.h>
 #include <time.h>
 
-void monnaie(double r) {
-    double b[] = {2000,1000,500,200,100,50,25,20,10,5,1,0.50,0.20,0.05};
-    char *n[] = {"Rs2000","Rs1000","Rs500","Rs200","Rs100","Rs50","Rs25",
-                    "Rs20","Rs10","Rs5","Rs1","50 sous","20 sous","5 sous"};
-    printf("Monnaie à rendre: %.2f Rs\n", r);
-    printf("Détails de votre monnaie:\n");
-    for(int i = 0; i < 14; i++) {
-        if(r <= 0)
-            break;
-        if(r >= b[i]) {
+double b[] = {2000, 1000, 500, 200, 100, 50, 25, 20, 10, 5, 1, 0.50, 0.20, 0.05};
+char *n[] = {"2000", "1000", "500", "200", "100", "50", "25", "20", "10", "5", "1", "0.50", "0.20", "0.05"};
+
+void monnaie(double r, int stock[]) {
+    printf("Rendu:\n");
+    for (int i = 0; i < sizeof(b) / sizeof(b[0]); i++) {
+        if (r >= b[i] && stock[i] > 0) {
             int nb = r / b[i];
-            printf("%d x %s\n", nb, n[i]);
-            r -= nb * b[i];
+            printf("%d x %s\n", nb > stock[i] ? stock[i] : nb, n[i]);
+            r -= (nb > stock[i] ? stock[i] : nb) * b[i];
+            stock[i] -= (nb > stock[i] ? stock[i] : nb);
         }
     }
+    if (r > 0.005) printf("Rendu impossible.\n");
+}
+
+int peut_rendre(double rendu, int stock[]) {
+    double temp_rendu = rendu;
+    int temp_stock[sizeof(b) / sizeof(b[0])];
+    for (int i = 0; i < sizeof(b) / sizeof(b[0]); i++) temp_stock[i] = stock[i];
+    for (int i = 0; i < sizeof(b) / sizeof(b[0]); i++)
+        if (temp_rendu >= b[i] && temp_stock[i] > 0)
+            temp_rendu -= (temp_rendu / b[i] > temp_stock[i] ? temp_stock[i] : (int)(temp_rendu / b[i])) * b[i];
+    return temp_rendu <= 0.005;
+}
+
+void remplir(int stock[]) {
+    printf("Remplissage:\n");
+    for (int i = 0; i < sizeof(b) / sizeof(b[0]); i++) {
+        int ajout;
+        printf("Nb %s: ", n[i]);
+        scanf("%d", &ajout);
+        stock[i] += ajout;
+    }
+    printf("Caisse remplie.\n");
 }
 
 int main() {
     srand(time(0));
-    double a = (rand() % 500000) / 100.0;
-    double p;
-    printf("Montant à payer: %.2f Rs\n", a);
-    do {
-        printf("Le montant que vous allez donner: ");
-        scanf("%lf", &p);
-        if(p < a) {
-            printf("Montant insuffisant!\n");
-        }
-    } while(p < a);
+    int stock[] = {2, 2, 2, 5, 5, 10, 10, 10, 15, 15, 20, 20, 20, 20};
+    int choix;
+    double a, p;
+    int continuer = 1;
 
-    if(p > a) {
-        monnaie(p - a);
-    } else {
-        printf("Pas de monnaie à rendre.\n");
+    printf("Stock initial:\n");
+    for (int i = 0; i < sizeof(b) / sizeof(b[0]); i++) 
+    	printf("%s: %d\n", n[i], stock[i]);
+
+    while (continuer) {
+        printf("\n A payer: %.2f Rs\nDonné: RS", (a = (rand() % 50000) / 100.0));
+        scanf("%lf", &p);
+        if (p != 1) {
+            printf("Erreur saisie.\n");
+            while (getchar() != '\n');
+            continue;
+        }
+
+        if (p >= a) {
+            if (p > a) {
+                if (peut_rendre(p - a, stock)) {
+                    monnaie(p - a, stock);
+                } else {
+                    printf("Rendu impossible.\n");
+                    printf("1 Fermer 2 Remplir Choix: ");
+                    if (scanf("%d", &choix) == 1) {
+                        continuer = 0;
+                    } else if (choix == 2) {
+                        remplir(stock);
+                    }
+                }
+            } else {
+                printf("Pas de monnaie à rendre.\n");
+            }
+        } else {
+            printf("Insuffisant!\n");
+        }
+
+        printf("Stock après:\n");
+        for (int i = 0; i < sizeof(b) / sizeof(b[0]); i++) 
+        	printf("%s: %d\n", n[i], stock[i]);
     }
+
+    printf("\nFin.\n");
     return 0;
 }
